105baf7d96599d212cefb869280f8199bf06e2b2
Esteban Solarte <estebansolarte86@gmail.com>
2023-06-22 01:19:18 -0500

    primera version del hook para guardar en el localStorage

---
+++ src/hooks/useLocalStorage.js
@@ -0,0 +1,38 @@
+import { useEffect, useState } from "react"
+
+const useLocalStorage = (key, initialValue) => {
+
+    const [data, setData] = useState(initialValue)
+    
+
+    useEffect(() => {
+
+        const data = localStorage.getItem(key)
+
+        !data 
+        ? localStorage.setItem(key, initialValue)
+        : setItems(JSON.parse(data))
+
+
+
+    }, [key, initialValue])
+
+
+    const saveData = (newData) => {
+        
+        const stringifyData = JSON.stringify(newData)
+        localStorage.setItem(key, newData)
+        setData(newData)
+        
+
+    }
+
+    return {
+        data,
+        saveData
+    }
+
+}
+
+
+export {useLocalStorage}
